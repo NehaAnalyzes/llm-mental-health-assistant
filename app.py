1fed3407d6539a6a69435fd6900baf56d6a5a31b
NehaAnalyzes <nehaedasseri@gmail.com>
2026-05-18 10:09:04 +0530

    migrated to groq llama version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_google_genai import ChatGoogleGenerativeAI
+
 import os
 from langchain_groq import ChatGroq
 
